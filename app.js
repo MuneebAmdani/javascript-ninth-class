eccc5949007e130cb66593a49aa8a13f70479738
MuneebAmdani <arhammuneeb239@gmail.com>
2024-11-14 13:09:42 +0500

    javascript ninth class

---
+++ app.js
@@ -0,0 +1,22 @@
+// let element=document.getElementsByTagName("h1");
+// let elementId=document.getElementById("second");
+// console.log(element)
+// console.log(elementId)
+
+// function changeColor(){
+//     let elementWithID=document.getElementById("shape");
+//     elementWithID.setAttribute("style","background-color:red;")
+//     console.log(elementWithID)
+// }
+
+// function changeColor(){
+//     let elementWithID=document.getElementById("shape");
+//    elementWithID.setAttribute("class","circle");
+//     console.log(elementWithID)
+// }
+
+// function changeColor(){
+//     let elementWithID=document.getElementById("shape").classList.toggle("hide");
+  
+//     console.log(elementWithID.classList)
+// }
